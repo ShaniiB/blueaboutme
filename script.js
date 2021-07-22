f8c5e7febb882f84655b55ba5f149c5ed0ae0e55
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-07-21 23:58:48 -0700

    fixed java

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+let askName = prompt("Hey there!\nWhat's your name?");
+    console.log("Their name is " + askName + "");
+
+ while (askName === "" || askName === null)
+  askName = prompt("Whoa there, stranger danger!\nYou're gonna give me that name.")
+
+  alert("" + askName + "? Cool.\nThanks for dropping by!");
+  alert("Actually..");
+   
+let getMoniker = prompt("Do you have a nickname?");
+    console.log("" + askName + " wants to be known as " + getMoniker + "");
+ 
+ while (getMoniker === "" || getMoniker === null)   
+  getMoniker = prompt("Don't you start this again" + askName + "!!\n") 
+
+  alert("" + getMoniker + "? Ok, Cool!\nYou can call me Shanii!");   
+
+let askBirthMonth = prompt("What's your birth month " + getMoniker + "?");
+  alert("" + askBirthMonth + "? Ok, Good to know!");
+    console.log('"' + getMoniker + '" was born in ' + askBirthMonth + '.');
+
+  alert("Alright " + getMoniker + "!\nLets see if you've paid attention to my git.")
+
+let myBirthMonth = prompt("Was I born in the Fall?");
+    console.log(myBirthMonth);
+ if (myBirthMonth === "yes") 
+{
+  alert("Oh wow! you do pay attention!");
+    console.log("" + getMoniker + "knows what of the season I was born!");
+} 
