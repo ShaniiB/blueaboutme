88cc24b0c4b71843ff724097fd10da7b0216bb08
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-07-24 23:05:46 -0700

    java re-added

--- script.js
+++ script.js
@@ -1,33 +1,110 @@
 "use strict"
-/*
-let askName = prompt("Hey there!\nWhat's your name?");
-    console.log("Their name is " + askName + "");
+//  asking the user for their name
+let askName = prompt("Whoa there, stranger danger!\nWhat's your name?");
+while (askName === "" || askName === null) { // user must enter ask the prommpt to move on
+  askName = prompt("Nope! You ain't entering until I get a name!");
 
- while (askName === "" || askName === null)
-  askName = prompt("Whoa there, stranger danger!\nYou're gonna give me that name.")
-
-  alert("" + askName + "? Cool.\nThanks for dropping by!");
-  alert("Actually..");
-   
-let getMoniker = prompt("Do you have a nickname?");
-    console.log("" + askName + " wants to be known as " + getMoniker + "");
- 
- while (getMoniker === "" || getMoniker === null)   
-  getMoniker = prompt("Don't you start this again" + askName + "!!\n") 
-
-  alert("" + getMoniker + "? Ok, Cool!\nYou can call me Shanii!");   
-
-let askBirthMonth = prompt("What's your birth month " + getMoniker + "?");
-  alert("" + askBirthMonth + "? Ok, Good to know!");
-    console.log('"' + getMoniker + '" was born in ' + askBirthMonth + '.');
-
-  alert("Alright " + getMoniker + "!\nLets see if you've paid attention to my git.")
-
-let myBirthMonth = prompt("Was I born in the Fall?");
-    console.log(myBirthMonth);
- if (myBirthMonth === "yes") 
+  //  successfully got their name to properly greet them
+  console.log(" Their name is " + askName + " ");
+}
+alert("Hey " + askName + "!\nThanks for dropping by!");
+alert("My name is Shanythia.\nNice to meet you!");
+alert("Actually..");
+alert("Got a nickname?");
+//  on second thought we want the user to enter their nickname 
+let getMoniker = prompt("What is it?");
+while (getMoniker === "" || getMoniker === null) {
+  alert("Seriously?"); // banter triggers when user doesn't enter their username
+  getMoniker = prompt("Enter nickname?");
+  alert("Stubborn aren't ya?");
+  alert("Ok..\nLets try this again..");
+  getMonkier = prompt("" + askName + ",\nWhat is your nickname?");
+  alert("OK! Whatever..\njust call me Shanii."); // user never learns your nickname as punishment
+  console.log(" " + askName + " is stubborn ");
+  break;
+}
+//  this is what triggers when user gives nickname @ prompt, you give them your nickname
 {
-  alert("Oh wow! you do pay attention!");
-    console.log("" + getMoniker + "knows what of the season I was born!");
-} 
-*/
+  if (getMoniker)
+    alert("" + getMoniker + "? Ok!\nFolks call me, Shanii or Bear!");
+  console.log("" + askName + " wants to be known as " + getMoniker + "");
+}
+
+//  time to challenge the user
+let challengeUser = true;
+
+while (challengeUser) {
+  let acceptChallenge = prompt("Do you accept my challenge?");
+  if (acceptChallenge.toUpperCase() === 'OK' || acceptChallenge.toUpperCase() === "YES" || acceptChallenge.toUpperCase() === "Y") {
+    challengeUser = false;
+    console.log("Challenge has been accepted.")
+  }
+}
+let askGamer = prompt("Do you like videogames?");
+if (askGamer === "Yes")
+  alert("Cool!\nI do too!"); {
+  if (askGamer === "Yes") {
+    askGamer = prompt("Do you play on Xbox?");
+    alert("Yesss! Shoot me your gamertag sometime!")
+
+    while (askGamer === "No") {
+      alert("Eh, to each their own.")
+    }
+  }
+}
+let askAnime = prompt("Do you like anime?");
+if (askAnime.toLowerCase === "yes")
+  alert("Saaame, I'm currently really into MHA\n(My Hero Academia)"); {
+  if (askAnime.toLowerCase === "yes") {
+    askAnime = prompt("Are you?");
+    alert("Bet! You're hella cool!")
+
+    while (askAnime === "no") {
+      alert("<_<..oh ok..")
+    }
+  }
+}
+// ask the user's age
+let askAge = prompt("Are you over the age of 18?");
+if (askAge === "yes")
+  alert("nice!"); {
+  alert("wait");
+  if (askAge === "yes") {
+    askAge = prompt("Are you over the age of 21?");
+    alert("Oh cool!")
+
+    while (askAge === "no") {
+      alert("Wait a minute..")
+      alert("You gotta be 18 to be in this program!");
+    }
+  }
+}
+// the user must guess your age
+let myAge = 26;
+
+for (let i = 18; i < 30; i++) {
+  let guessAge = prompt("How old do you think I am?");
+  if (guessAge == myAge) {
+    alert("Das riiight! ~ '94!");
+    break;
+  }
+  else if (guessAge > myAge) {
+    alert("Nope!!")
+  }
+  else if (guessAge < myAge) {
+    alert("Flattering!~ But nope");
+  }
+}
+// user must guess my favorite animal
+let answers = ["Bears"];
+for (let i = -3; i < 3; i++) {
+  let questionInput = prompt("Between these animals can you guess my favorite?\nHyenas, Lions, Bears, Wolves, Goats, Skinks, and Ducks");
+  if (answers.indexOf(questionInput) == -3) {
+    alert("Nope.~");
+    alert("Guess again!")
+  }
+  else {
+    alert("You know me so well.~");
+    break;
+  }
+}
